0dd95b528d39cef575225e3e22305d45bfa7c20d
Alex <harding1996@gmail.com>
2021-05-14 15:28:09 -0600

    moved LoginComponent into UserModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [NavbarComponent, LoginComponent],
+  declarations: [NavbarComponent],
   imports: [SharedModule],
   exports: [NavbarComponent],
 })
