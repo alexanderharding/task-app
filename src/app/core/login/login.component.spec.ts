f491ad9cac469b873b87a0507309903064a5521a
Alex <harding1996@gmail.com>
2021-05-13 22:13:08 -0600

    set up starter tests for LoginComponent

--- src/app/core/login/login.component.spec.ts
+++ src/app/core/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { LoginComponent } from './login.component';
 
@@ -21,5 +22,41 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
 
     expect(component).toBeTruthy();
+  });
+
+  it('should have set pageTitle correctly', () => {
+    fixture.detectChanges();
+
+    expect(component.pageTitle).toBe('Login');
+  });
+});
+
+describe('LoginComponent w/ template', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  });
+
+  it('should set the pageTitle in the template', () => {
+    fixture.detectChanges();
+
+    const elements = fixture.debugElement.queryAll(By.css('h1'));
+    expect(elements.length).toBe(1);
+    expect(elements[0].nativeElement.textContent).toBe('Login');
   });
 });
