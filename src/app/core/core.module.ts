db469f934d300c0bace5621562b2832fac6318ac
Alex <harding1996@gmail.com>
2021-05-13 20:41:58 -0600

    generated LoginComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, LoginComponent],
   imports: [SharedModule],
   exports: [NavbarComponent],
 })
