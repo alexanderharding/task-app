db6df2788d4896b854477358fb7bc754e14b5f31
Alex <harding1996@gmail.com>
2021-05-13 20:30:18 -0600

    created FakeNabarComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
+import { Component, Input } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  @Component({
+    selector: 'core-navbar',
+    template: '<div></div>',
+  })
+  class FakeNabarComponent {
+    @Input() pageTitle = '';
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, FakeNabarComponent],
     }).compileComponents();
   });
 
@@ -16,7 +25,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task app'`, () => {
+  it(`should have title as 'task app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('task app');
