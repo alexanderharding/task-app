35f8fd0c761e6e0c450cd0574bd2fc1cb78efdc1
Alex <harding1996@gmail.com>
2021-05-13 20:33:23 -0600

    created basic testing for model and template

--- src/app/core/navbar/navbar.component.spec.ts
+++ src/app/core/navbar/navbar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -8,18 +9,58 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+      declarations: [NavbarComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+  });
+
+  it('should set pageTitle correctly', () => {
+    fixture.detectChanges();
+
+    expect(component.pageTitle).toBe('');
+  });
+});
+
+describe('NavbarComponent w/ template', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  const TITLE = 'task app';
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NavbarComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  });
+
+  it('should set the pageTitle in the template', () => {
+    component.pageTitle = TITLE;
+
+    fixture.detectChanges();
+
+    const elements = fixture.debugElement.queryAll(By.css('nav a'));
+    expect(elements.length).toBe(1);
+    expect(elements[0].nativeElement.textContent).toBe(TITLE);
   });
 });
